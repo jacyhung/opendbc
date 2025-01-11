0002126ee6316360a9d86f0197f5ad739c7f85c9
Jacy Hung <jacy@Jacys-MacBook-Pro.local>
2025-01-11 11:16:52 -0800

    temp: increase speed

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -34,12 +34,12 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 3
 
       if CP.flags & HyundaiFlags.CCNC:
-        self.STEER_MAX = 384 if vEgoRaw < 11. else 330
+        self.STEER_MAX = 384 if vEgoRaw < 15 else 330
         self.STEER_DRIVER_ALLOWANCE = 350
         self.STEER_DRIVER_MULTIPLIER = 2
         self.STEER_THRESHOLD = 350
-        self.STEER_DELTA_UP = 10 if vEgoRaw < 11. else 2
-        self.STEER_DELTA_DOWN = 10 if vEgoRaw < 11. else 3
+        self.STEER_DELTA_UP = 10 if vEgoRaw < 15 else 2
+        self.STEER_DELTA_DOWN = 10 if vEgoRaw < 15 else 3
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
